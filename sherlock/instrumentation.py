cc6549f7f2390f31beb878ef1668af22bbec2ae7
Akshat <akshat@markovml.com>
2024-03-13 15:46:38 +0530

    Add requests and fastapi integrations.
    Signed-off-by: Akshat <akshat@markovml.com>

---
+++ sherlock/instrumentation.py
@@ -0,0 +1,25 @@
+from contextvars import ContextVar
+from typing import Dict
+from uuid import uuid4
+
+from sherlock.constants import CORRELATION_ID_NAME
+
+correlation_id: ContextVar[str] = ContextVar("correlation_id", default=uuid4().hex)
+
+
+def get_correlation_id_header(generate_new: bool = False) -> Dict[str, str]:
+    """
+    Returns Correlation ID header
+
+    :param generate_new: If to generate new correlation ID
+    :type generate_new: bool
+    :return: Correlation ID Header
+    :rtype: Dict[str, str]
+    """
+    if generate_new:
+        set_correlation_id(uuid4().hex)
+    return {CORRELATION_ID_NAME: correlation_id.get()}
+
+
+def set_correlation_id(value: str) -> None:
+    correlation_id.set(value)
---
+++ sherlock/integrations/fastapi.py
@@ -0,0 +1,50 @@
+from sherlock.constants import CORRELATION_ID_NAME
+from sherlock.instrumentation import get_correlation_id_header, set_correlation_id
+from sherlock.integrations.integration import AbstractIntegration
+
+try:
+    import fastapi
+    from fastapi import Response
+except ImportError:
+    pass
+
+
+class FastAPIIntegration(AbstractIntegration):
+    def __init__(self) -> None:
+        old_get_request_handler = fastapi.routing.get_request_handler
+
+        def new_get_request_handler(*args, **kwargs):
+            old_app = old_get_request_handler(*args, **kwargs)
+
+            async def new_app(*args, **kwargs):
+                request = args[0]
+                headers = dict(request.scope["headers"])
+                old_correlation_id = headers.get(CORRELATION_ID_NAME)
+
+                if old_correlation_id is not None:
+                    set_correlation_id(old_correlation_id)
+                    correlation_id_header = get_correlation_id_header()
+                else:
+                    correlation_id_header = get_correlation_id_header(generate_new=True)
+                    headers.update(correlation_id_header)
+
+                request.scope["headers"] = [(k, v) for k, v in headers.items()]
+
+                new_correlation_id = correlation_id_header[CORRELATION_ID_NAME]
+                response: Response = await old_app(*args, **kwargs)
+                if CORRELATION_ID_NAME not in response.headers.keys():
+                    response.raw_headers.append(
+                        (
+                            str.encode(CORRELATION_ID_NAME),
+                            str.encode(new_correlation_id),
+                        )
+                    )
+                return response
+
+            return new_app
+
+        fastapi.routing.get_request_handler = new_get_request_handler
+
+    @classmethod
+    def integration_name(cls) -> str:
+        return "fastapi"
