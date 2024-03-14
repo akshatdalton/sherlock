b39fb718477955da2542f656064b5bae3ca71075
Akshat <akshat@markovml.com>
2024-03-14 16:06:25 +0530

    Auto detect and enable available libraries integrations.
    Signed-off-by: Akshat <akshat@markovml.com>

--- sherlock/sleuth.py
+++ sherlock/sleuth.py
@@ -1,13 +1,30 @@
+from importlib import metadata as importlib_metadata
+from typing import Dict, List
+
 from sherlock.integrations.fastapi import FastAPIIntegration
 from sherlock.integrations.httpx import HttpxIntegration
 from sherlock.integrations.requests import RequestsIntegration
 
+AVAILABLE_INTEGRATIONS: Dict = {
+    RequestsIntegration.integration_name: RequestsIntegration,
+    FastAPIIntegration.integration_name: FastAPIIntegration,
+    HttpxIntegration.integration_name: HttpxIntegration,
+}
+
+
+def _get_installed_libraries() -> List[str]:
+    return [dist.metadata["Name"] for dist in importlib_metadata.distributions()]
+
 
 def _setup_integrations() -> None:
-    # TODO: Add logic to detect installed libraries and accordingly set up the corresponding integrations.
-    RequestsIntegration()
-    FastAPIIntegration()
-    HttpxIntegration()
+    installed_libraries_set = set(_get_installed_libraries())
+    available_libraries_integration_set = set(AVAILABLE_INTEGRATIONS.keys())
+
+    for found_library in installed_libraries_set.intersection(
+        available_libraries_integration_set
+    ):
+        integration = AVAILABLE_INTEGRATIONS[found_library]
+        integration()
 
 
 def sleuth() -> None:
