768b3f4067f05da629fa13ca43ae28a0a1326c95
Akshat <akshat@markovml.com>
2024-03-26 12:50:45 +0530

    Update test requirements.
    Signed-off-by: Akshat <akshat@markovml.com>

--- tests/test_correlation_id_generator.py
+++ tests/test_correlation_id_generator.py
@@ -2,7 +2,6 @@ import unittest
 from uuid import uuid4
 
 import requests
-import responses
 
 from sherlock import sleuth
 from sherlock.constants import CORRELATION_ID_NAME
@@ -19,15 +18,9 @@ class TestCorrelationIDGeneratorIntegration(unittest.TestCase):
     def tearDown(self):
         sleuth(correlation_id_generator_func=lambda: uuid4().hex)
 
-    @responses.activate
     def test_custom_correlation_id_generator(self):
-        responses.get("https://google.com")
-
         response = requests.get("https://google.com")
-        request = responses.calls[0].request
 
-        self.assertIn(CORRELATION_ID_NAME, request.headers)
         self.assertIn(CORRELATION_ID_NAME, response.headers)
-        self.assertEqual(request.headers.get(CORRELATION_ID_NAME)[:3], "tr-")
         self.assertEqual(response.headers.get(CORRELATION_ID_NAME)[:3], "tr-")
         self.assertEqual(200, response.status_code)
